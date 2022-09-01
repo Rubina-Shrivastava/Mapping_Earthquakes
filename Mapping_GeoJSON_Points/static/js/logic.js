4184783cd0c234f945143927538a660d1a0fd98a
Rubina-Shrivastava <106940766+Rubina-Shrivastava@users.noreply.github.com>
2022-08-31 21:53:06 -0500

    updating json path

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -39,7 +39,7 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 // let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
-let airportData = "C:\Users\rubin\Mapping_Earthquakes\majorAirports.json";
+let airportData = "https://github.com/Rubina-Shrivastava/Mapping_Earthquakes/blob/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
